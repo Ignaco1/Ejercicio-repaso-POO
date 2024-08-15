04623b5856f430cbef0bbe0759b8072a3fb09a1b
Navegador <Navegador@RLG10703-83516>
2024-08-15 10:29:42 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio repaso POO/Form_registro.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Ejercicio_repaso_POO
+{
+    partial class Form_registro
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            cb_tipocuenta = new ComboBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox2 = new TextBox();
+            label6 = new Label();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(88, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Tipo de cuenta:";
+            // 
+            // cb_tipocuenta
+            // 
+            cb_tipocuenta.FormattingEnabled = true;
+            cb_tipocuenta.Location = new Point(149, 26);
+            cb_tipocuenta.Name = "cb_tipocuenta";
+            cb_tipocuenta.Size = new Size(121, 23);
+            cb_tipocuenta.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 70);
+            label2.Name = "label2";
+            label2.Size = new Size(30, 15);
+            label2.TabIndex = 2;
+            label2.Text = "DNI:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(149, 67);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(121, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 114);
+            label3.Name = "label3";
+            label3.Size = new Size(54, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Nombre:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 155);
+            label4.Name = "label4";
+            label4.Size = new Size(55, 15);
+            label4.TabIndex = 5;
+            label4.Text = "Telefono:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 197);
+            label5.Name = "label5";
+            label5.Size = new Size(39, 15);
+            label5.TabIndex = 6;
+            label5.Text = "Email:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(149, 111);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(121, 23);
+            textBox2.TabIndex = 7;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 238);
+            label6.Name = "label6";
+            label6.Size = new Size(120, 15);
+            label6.TabIndex = 8;
+            label6.Text = "Fecha de nacimiento:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(149, 152);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(121, 23);
+            textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(149, 194);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(121, 23);
+            textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(149, 235);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(121, 23);
+            textBox5.TabIndex = 11;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(212, 305);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 36);
+            button1.TabIndex = 12;
+            button1.Text = "Registrar cliente";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form_registro
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(344, 353);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(label6);
+            Controls.Add(textBox2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(cb_tipocuenta);
+            Controls.Add(label1);
+            Name = "Form_registro";
+            Text = "Form de registro de clientes";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox cb_tipocuenta;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox2;
+        private Label label6;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Button button1;
+    }
+}
